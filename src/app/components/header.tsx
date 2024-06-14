f2763d40e698328ec8197053a045cc49f9fe9f4b
micho <michomhakim09@gmail.com>
2024-06-14 17:21:03 +0300

    first commit

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full bg-yellow-500 flex justify-between">
+      <div className="text-white text-xl p-2 font-bold font-serif" style={{marginLeft:"600px",fontSize:"30px"}}>Todo App</div>
+    </div>
+  );
+};
+
+export default Header;
